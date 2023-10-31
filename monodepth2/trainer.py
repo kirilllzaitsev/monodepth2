27e3e4e7921267462566ff10db971a53fbb84a3b
kirilllzaitsev <kzaitsev88@gmail.com>
2023-10-31 17:34:58 +0100

    minor

--- monodepth2/trainer.py
+++ monodepth2/trainer.py
@@ -48,6 +48,8 @@ class Trainer:
         self.exp.add_tag("monodepth2")
         self.exp.add_tag("resnet18")
         self.exp.add_tag("overfit" if self.opt.do_overfit else "full")
+        if self.opt.use_df_rec_loss:
+            self.exp.add_tag("df_rec_loss")
 
         # checking height and width are multiples of 32
         assert self.opt.height % 32 == 0, "'height' must be a multiple of 32"
