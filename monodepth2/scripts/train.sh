26b433a70f7d877c1283a27d8dfefe8ff3b7e22b
kirilllzaitsev <kzaitsev88@gmail.com>
2023-10-31 19:06:09 +0100

    minor fix

--- monodepth2/scripts/train.sh
+++ monodepth2/scripts/train.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# REQUIRES having predictions exported to /tmp/artifacts/${exp_name}/eval/${eval_ds}/pred_disps.npy
-
 data_path=/kitti_raw_data
 
-exp_name=stiff_wolverine_3619  # experiment name on comet, https://www.comet.com/kirilllzaitsev/layout-aware-monodepth/59e313a8f5de4d37899eaa988655bfaa
-exp_path=/tmp/artifacts/${exp_name}
-eval_ds=kitti
-# eval_ds=waymo
-# eval_ds=argoverse
+# --use_line_loss --use_modelip_loss flags mean that both losses are used
 
-python evaluate_depth.py --ds ${eval_ds} --ext_disp_to_eval ${exp_path}/eval/${eval_ds}/pred_disps.npy --png --eval_mono --load_weights_folder ${exp_path} --data_path ${data_path} --eval_split eigen --eval_out_dir ${exp_path}/eval
+python train.py --exp_disabled --exp_tags debug --use_line_loss --use_modelip_loss --model_name mono_model --png --split eigen_full --data_path ${data_path} --batch_size 8 --log_frequency 32 --num_workers 8
