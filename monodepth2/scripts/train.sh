ec0279126624ca4bbecc1ab5e4a977a41b3110ca
kirilllzaitsev <kzaitsev88@gmail.com>
2023-10-31 20:43:14 +0100

    add --modelip_loss_scale 2

--- monodepth2/scripts/train.sh
+++ monodepth2/scripts/train.sh
@@ -4,4 +4,4 @@ data_path=/kitti_raw_data
 
 # --use_line_loss --use_modelip_loss flags mean that both losses are used
 
-python train.py --exp_disabled --exp_tags debug --use_line_loss --use_modelip_loss --use_df_rec_loss --filter_lines angle+length --model_name mono_model --png --split eigen_full --data_path ${data_path} --batch_size 8 --log_frequency 32 --num_workers 8
+python train.py --exp_disabled --exp_tags debug --use_line_loss --use_modelip_loss --use_df_rec_loss --filter_lines angle+length --modelip_loss_scale 2 --model_name mono_model --png --split eigen_full --data_path ${data_path} --batch_size 8 --log_frequency 32 --num_workers 8
